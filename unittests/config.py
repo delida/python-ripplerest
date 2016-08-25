826995bb5915c5657d77809c0db308005e729aae
delida <Qing@QingdeMacBook-Pro.local>
2016-08-24 19:44:38 -0700

    unittest
    unittest

--- unittests/config.py
+++ unittests/config.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
-server_host = "tapi.jingtum.com" #"kapi.jingtum.com"
-sercer_port = 443
+server_host = "tapi.jingtum.com" #"101.200.230.74" #"123.57.209.177" #"101.200.174.147" #"kapi.jingtum.com" 
+sercer_port = 443 #8001 #80
 is_https = True
+web_socket_address = "wss://tapi.jingtum.com:5443" #"ws://101.200.230.74:5443" #"ws://123.57.209.177:5445" #"ws://101.200.174.147:5443" 
 
 #testaddress = "j3x4oo2KvexqjBoG7J9HXYjpFz8cLT31B9"
 
-issuer_account = "jUFZJJnTbwBzuZcSuWc2nMFNpaGeWtRCD6"
-issuer_secret = "ssKt7P86m5PBYmMAJoay6YZ1gNTvq"
+issuer_account = "jHb9CJAWyB4jr91VRWn96DkukG4bwdtyTh"
+issuer_secret = "snoPBjXtMeMyMHUVTgbuqAfg1SUTb"
 issuer = "jBciDE8Q3uJjf111VeiUNM775AMKHEbBLS"
+
+currency_ulimit_account = "jJ8PzpT7er3tXEWaUsVTPy3kQUaHVHdxvp"
+currency_ulimit_secret = "shYK7gZVBzw4m71FFqZh9TWGXLR6Q"
--- unittests/jtunittest.py
+++ unittests/jtunittest.py
@@ -7,6 +7,10 @@ import time, datetime
 
 from ripplerest.client import Client 
 import config
+import websocket
+from websocket import create_connection
+import json
+import threading
 
 def exeTime(func):  
     def _func(*args, **args2):  
@@ -16,9 +20,51 @@ def exeTime(func):
         return back  
     return _func 
 
+class WebSocketClient:
+    def __init__(self, ws_address):
+        self._shutdown = True
+        self.ws = create_connection(ws_address)
+        print self.ws.recv()#, self.ws.__dict__
+
+    # def __del__(self):
+    #     print "WebSocketClient __del__", self.close()
+
+    def send(self, data):
+        ret = None
+        data = json.dumps(data).encode('utf-8')
+        try:
+            self.ws.send(data)
+            ret = self.ws.recv()
+        except Exception, e:
+            print "websocket send error", e
+        return ret
+
+    def subscribe_message_by_account(self, address, secret):
+        _data = {
+            "command": "subscribe",
+            "account": address,
+            "secret": secret 
+        }
+        return self.send(_data)
+
+    def close(self):
+        print "close..."
+        _data = {
+            "command": "close",
+        }
+        self._shutdown = False
+        return self.send(_data) 
+
+    def receive(self):
+        try:    
+            msg = json.loads(self.ws.recv().decode('utf-8'))
+            print 'websocket<<<<<<<< receiving % s', json.dumps(msg, indent=2)
+        except Exception, e:
+            print e
+        
 class JingtumTestCase(unittest.TestCase):
     def setUp(self):
-        self.clienthelper = Client(config.server_host + ":" + str(config.sercer_port), config.is_https)        
+        self.clienthelper = Client(config.server_host + ":" + str(config.sercer_port), config.is_https)  
 
     def tearDown(self):
         pass
@@ -52,7 +98,7 @@ class JingtumTestCase(unittest.TestCase):
         ret_dict = None
         _address, _secret = self._priGetInfoFromDB()
         try:
-            ret_dict = self.clienthelper.active_account("SWT", 28, config.issuer_account, 
+            ret_dict = self.clienthelper.active_account("SWT", 100000000, config.issuer_account, 
                 _address, config.issuer_secret, None)
         except Exception, e:
             print "active_account error", e
@@ -62,25 +108,33 @@ class JingtumTestCase(unittest.TestCase):
         print "[testActiveAccount]", ret_dict
 
     def test_S024_Sleep(self):
-        time.sleep(5)
+        time.sleep(10)
         self.assertEqual(True, True)
 
     @exeTime     
     def test_S034_ActiveAccount(self):
         ret_dict = None
         _address, _secret = self._priGetInfoFromDB()
+
+        websocket_handler = WebSocketClient(config.web_socket_address)  
+        websocket_handler.subscribe_message_by_account(_address, _secret) 
+        t = threading.Thread(target=websocket_handler.receive, args=())
+        t.setDaemon(False)
+        t.start()
+
         try:
-            ret_dict = self.clienthelper.active_account("USD", 1, config.issuer_account, 
-                _address, config.issuer_secret, config.issuer)
+            ret_dict = self.clienthelper.active_account("USD", 5, config.currency_ulimit_account, 
+                _address, config.currency_ulimit_secret, config.issuer)
         except Exception, e:
             print "active_account error", e
 
         self.assertEqual(ret_dict.has_key("success") and ret_dict["success"], True)
 
-        print "[testActiveAccount]", ret_dict
+        print "[testActiveAccount_ws]", ret_dict
+        
 
     def test_S043_Sleep(self):
-        time.sleep(5)
+        time.sleep(10)
         self.assertEqual(True, True)
     
     @exeTime    
@@ -122,19 +176,19 @@ class JingtumTestCase(unittest.TestCase):
             
         print "[testPlaceOrder]", _ret_dict
 
-    @exeTime
-    def test_S065_RetrieveOrderTransaction(self):
-        _ret_dict = {}
-        _address, _secret = self._priGetInfoFromDB()
-        fo = open("tmphash.txt", "r")
-        hash_id = fo.readline()
-        fo.close()
-        try:
-           _ret_dict = self.clienthelper.retrieve_order_transaction(_address, hash_id)
-        except Exception, e:
-            print "retrieve_order_transaction error", e  
+    # @exeTime
+    # def test_S065_RetrieveOrderTransaction(self):
+    #     _ret_dict = {}
+    #     _address, _secret = self._priGetInfoFromDB()
+    #     fo = open("tmphash.txt", "r")
+    #     hash_id = fo.readline()
+    #     fo.close()
+    #     try:
+    #        _ret_dict = self.clienthelper.retrieve_order_transaction(_address, hash_id)
+    #     except Exception, e:
+    #         print "retrieve_order_transaction error", e  
         
-        print "[testRetrieveOrderTransaction]", _ret_dict
+    #     print "[testRetrieveOrderTransaction]", _ret_dict
 
     @exeTime
     def test_S071_GetOrders(self):
@@ -246,44 +300,162 @@ class JingtumTestCase(unittest.TestCase):
         
         print "[testDeleteRelations]", _ret_dict
 
-    # @exeTime
-    # def test_S990_PlaceOrder(self):
-    #     _ret_dict = {}
-
-    #     _address, _secret = self._priGetInfoFromDB()
-    #     try:
-    #         _ret_dict = self.clienthelper.place_order(_address, _secret, "buy", "SWT", 2, 
-    #             "USD", 1, None, config.issuer)
-    #     except Exception, e:
-    #         print "place_order error", e  
-
-    #     self.assertEqual(_ret_dict.has_key("success") and _ret_dict["success"], True)
-
-    #     fo = open("tmphash.txt", "w+")
-    #     fo.write(_ret_dict["hash"])
-    #     fo.close()
-            
-    #     print "[testPlaceOrder2]", _ret_dict
-
     @exeTime    
-    def test_S951_GetBalance(self):
-        _ret = []
+    def test_S151_PostSettings(self):
+        _ret_dict = {}
 
         _address, _secret = self._priGetInfoFromDB()
         try:
-            _g = self.clienthelper.get_balances(_address)
+            _settings = {"settings" : {"regular_key": config.currency_ulimit_account}}
+            _ret_dict = self.clienthelper.post_account_settings(_address, _secret, **_settings)
         except Exception, e:
-            print "get_balances error", e 
+            print "post_account_settings error", e 
             
-        while 1:
-            try:
-                _res = _g.next()
-                _ret.append(_res)
-            except StopIteration:
-                break
+        print "[test_S151_PostSettings]", _ret_dict
 
-        print "[testGetBalance]", _ret
-        self.assertNotEqual(len(_ret), 0)
+    @exeTime    
+    def test_S161_GetSettings(self):
+        _ret_dict = {}
+
+        _address, _secret = self._priGetInfoFromDB()
+        try:
+            _ret_dict = self.clienthelper.get_account_settings(_address)
+        except Exception, e:
+            print "get_account_settings error", e 
+            
+
+        print "[test_S161_GetSettings]", _ret_dict
+
+    @exeTime    
+    def test_S171_PostSettings2(self):
+        _ret_dict = {}
+
+        _address, _secret = self._priGetInfoFromDB()
+        try:
+            _signers = {"signers" : {"master_weight": 10, "quorum": 20, "signer_entries": 
+                {"account" : config.currency_ulimit_account, "weight": 5}}}
+            _ret_dict = self.clienthelper.post_account_settings(_address, _secret, **_signers)
+        except Exception, e:
+            print "post_account_settings2 error", e 
+            
+        print "[test_S171_PostSettings2]", _ret_dict
+
+    @exeTime    
+    def test_S181_GetSigners(self):
+        _ret_dict = {}
+
+        _address, _secret = self._priGetInfoFromDB()
+        try:
+            _ret_dict = self.clienthelper.get_account_signers(_address)
+        except Exception, e:
+            print "get_account_signers error", e 
+            
+
+        print "[test_S181_GetSigners]", _ret_dict
+
+    @exeTime    
+    def test_S900_PostOperations(self):
+        _ret_dict = {}
+
+        _address, _secret = self._priGetInfoFromDB()
+        try:
+            _signers = [{"secret": _secret}, {"secret": config.issuer_secret} ]
+            _operations = [
+                {
+                    "account": _address,
+                    "type": "SetRegularKey",
+                    "settings": {
+                        "regular_key": config.currency_ulimit_account
+                    }
+                },
+                {
+                    "account": _address,
+                    "type": "sell",
+                    "order": {
+                        "type": "sell",
+                        "taker_gets": {
+                            "currency": "USD",
+                            "counterparty": config.issuer,
+                            "value": "1"
+                        },
+                        "taker_pays": {
+                            "currency": "SWT",
+                            "value": "20",
+                        }
+                    },
+                },
+                {
+                    "account": _address,
+                    "type": "TrustSet",
+                    "trustline": {
+                        "limit": "1",
+                        "currency": "USD",
+                        "counterparty": config.issuer,
+                        "account_trustline_frozen": True
+                    }
+                },
+                {
+                    "type": "Payment",
+                    "account": config.issuer_account,
+                    "payment": {
+                        "source_account": config.issuer_account,
+                        "destination_amount": {
+                            "currency": "SWT",
+                            "issuer": "",
+                            "value": "200"
+                        },
+                        "destination_account": _address,
+                        "paths": "[]"
+                    }
+                }
+       
+
+            ]
+            _ret_dict = self.clienthelper.post_operations(_address, _secret, _signers, _operations)
+        except Exception, e:
+            print "post_operations error", e 
+            
+        print "[test_S900_PostOperations]", _ret_dict
+
+
+    # # @exeTime
+    # # def test_S990_PlaceOrder(self):
+    # #     _ret_dict = {}
+
+    # #     _address, _secret = self._priGetInfoFromDB()
+    # #     try:
+    # #         _ret_dict = self.clienthelper.place_order(_address, _secret, "buy", "SWT", 2, 
+    # #             "USD", 1, None, config.issuer)
+    # #     except Exception, e:
+    # #         print "place_order error", e  
+
+    # #     self.assertEqual(_ret_dict.has_key("success") and _ret_dict["success"], True)
+
+    # #     fo = open("tmphash.txt", "w+")
+    # #     fo.write(_ret_dict["hash"])
+    # #     fo.close()
+            
+    # #     print "[testPlaceOrder2]", _ret_dict
+
+    # @exeTime    
+    # def test_S951_GetBalance(self):
+    #     _ret = []
+
+    #     _address, _secret = self._priGetInfoFromDB()
+    #     try:
+    #         _g = self.clienthelper.get_balances(_address)
+    #     except Exception, e:
+    #         print "get_balances error", e 
+            
+    #     while 1:
+    #         try:
+    #             _res = _g.next()
+    #             _ret.append(_res)
+    #         except StopIteration:
+    #             break
+
+    #     print "[testGetBalance]", _ret
+    #     self.assertNotEqual(len(_ret), 0)
 
 
     @exeTime    
@@ -304,10 +476,10 @@ class JingtumTestCase(unittest.TestCase):
                 break
 
         print "[MasterBalance]", _ret
-        self.assertNotEqual(len(_ret), 0)
-
-    
+        self.assertNotEqual(len(_ret), 0) 
 
 if __name__ == "__main__":
     print "current_time:::", str(datetime.datetime.fromtimestamp(int(time.time())))
+
     unittest.main()
+    
